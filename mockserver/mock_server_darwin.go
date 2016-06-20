943f33f0fd66e95df535a6056a7c564911b4e9c2
Matt Fellows <matt.fellows@onegeek.com.au>
2016-06-20 22:08:18 +1000

    WIP

---
+++ mockserver/mock_server_darwin.go
@@ -0,0 +1,34 @@
+package mockserver
+
+/*
+#cgo LDFLAGS: ${SRCDIR}/../libs/libpact_mock_server.dylib
+
+// Library headers
+int create_mock_server(char* pact, int port);
+*/
+import "C"
+import (
+	"fmt"
+	"time"
+)
+
+func CreateMockServer() {
+	res := C.create_mock_server(C.CString(`
+		{
+		  "consumer": {
+		    "name": "billy"
+		  },
+		  "provider": {
+		    "name": "bobby"
+		  },
+		  "interactions": [
+
+		  ],
+		  "metadata": {
+		    "pactSpecificationVersion": "2.0.0"
+		  }
+		}
+	`), 0)
+	fmt.Println("Mock server running on port Result:", res)
+	<-time.After(3000 * time.Second)
+}
